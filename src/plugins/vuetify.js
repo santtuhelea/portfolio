7130bdd2643c387c3c3ebe724eaefb8fdd524020
Santtu Heleä <santtu.helea@triplan.fi>
2020-03-15 19:50:31 +0200

    Task: Add prettier-config

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,18 @@
-import Vue from "vue";
-import Vuetify from "vuetify/lib";
+import Vue from 'vue'
+import Vuetify, { colors } from 'vuetify/lib'
 
-Vue.use(Vuetify);
+Vue.use(Vuetify)
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: colors.yellow,
+      },
+      dark: {},
+    },
+    options: {
+      customProperties: true,
+    },
+  },
+})
